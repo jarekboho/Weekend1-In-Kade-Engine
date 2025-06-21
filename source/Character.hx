e8de9087e8e971535bdfc2bf478987e04f77d5d0
jarekboho <123318901+jarekboho@users.noreply.github.com>
2025-06-22 00:52:48 +0300

    Update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -262,6 +262,8 @@ animation.addByIndices('laughCutscene', 'Laugh0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
 				addOffset('lowerKnife', 135, 52);
 
 				playAnim('danceRight');
+
+				globalOffsets[1] = -100;
 		}
 
 		dance();
