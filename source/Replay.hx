9013da380671af32cce0c2d13b8d5ae181c4a593
jarekboho <123318901+jarekboho@users.noreply.github.com>
2025-08-05 13:34:38 +0300

    Update Replay.hx

--- source/Replay.hx
+++ source/Replay.hx
@@ -35,7 +35,7 @@ typedef ReplayJSON =
 
 class Replay
 {
-    public static var version:String = "1.0"; // replay file version
+    public static var version:String = "1.0";
 
     public var path:String = "";
     public var replay:ReplayJSON;
