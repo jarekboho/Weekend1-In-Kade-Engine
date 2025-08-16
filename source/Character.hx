3cf622772c2436409e4288b56dce8ef6c0512426
jarekboho <123318901+jarekboho@users.noreply.github.com>
2025-08-16 03:12:04 +0300

    Update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -303,7 +303,6 @@ animation.addByIndices('burpSmileLong', 'burpsmile', [0, 1, 2, 3, 4, 5, 6, 7, 8,
 
 				frames = texture;
 
-				animation.addByPrefix('idle', 'Idle0', 24, false);
 				animation.addByIndices('danceLeft', 'Idle0', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
 				animation.addByIndices('danceRight', 'Idle0', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
 				animation.addByPrefix('combo50', 'ComboCheer0', 24, false);
